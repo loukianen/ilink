e49de435d85727c58045185452d9969026adc8d1
Konstantin Lukyanenok <loukianen@gmail.com>
2022-04-05 19:55:27 +0300

    add Header

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from '../header/Header';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className="app">
+      <Header />
     </div>
   );
 }
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,25 @@
+import './header.css';
+
+function Header() {
+  const handleControlPanelButtonClick = () => {
+    alert('Здесь будет панель управления');
+  };
+
+  return (
+    <header>
+      <div className='header_avatar'>
+        <img src="img/avatar.jpg" className="avatar_img" alt="avatar" />
+        <span>Константин</span>
+        <span className="last-name">Лукьяненок</span>
+      </div>
+      <div className='logo'>
+        <img src="img/ilink-logo.png" className="logo_ilink" alt="logo" />
+        <img src="img/academy-logo.png" className="logo_academy" alt="logo" />
+      </div>
+      <button type="button" className="header_menu-button button" onClick={handleControlPanelButtonClick}>Панель управления</button>
+      <button type="button" className="header_menu-button__mobile button" onClick={handleControlPanelButtonClick}></button>
+    </header>
+  );
+}
+
+export default Header;
