23e50d9ccd6a782e7ac36fa26ccf25f6617e338f
Konstantin Lukyanenok <loukianen@gmail.com>
2022-04-12 16:50:39 +0300

    add file size validation

--- src/components/file-loading/FileLoading.jsx
+++ src/components/file-loading/FileLoading.jsx
@@ -1,16 +1,55 @@
+import { useCallback, useEffect } from 'react';
 import './file-loading.css';
 
+const MAX_PROGRESS = 100;
+const warningFileMessage = 'Your file is too big!';
+
+const getFileInfo = (files, loadingState) => {
+  if (loadingState === 'failed') {
+    return warningFileMessage;
+  }
+  return files[0].name;
+};
+
 function FileLoading(props) {
+  const { loadingState, files, onCancelChoosingFile, onSetFileLoadingState } = props;
+  const fileInfo = getFileInfo(files, loadingState);
+  const spinerVisibilityClass = loadingState === 'started' ? '' : ' hidden';
+  const pailVisibilityClass = loadingState === 'finished' || loadingState === 'failed' ? '' : ' hidden';
+  const fileInfoClass = loadingState === 'failed' ? ' warning' : '';
+  const progressBarClass = loadingState === 'failed' ? ' warning' : '';
+
+  const handlePailClick = () => {
+    onCancelChoosingFile();
+  }
+
+  const startProgressBarAnimation = useCallback(() => {
+    const progressbar = document.getElementById('file-loading_progress-bar');
+    const intervalId = setInterval(tick, 10);
+    function tick() {
+      if (progressbar.value >= progressbar.max) {
+        onSetFileLoadingState('finished');
+        clearInterval(intervalId);
+      }
+      progressbar.value = progressbar.value + 1;
+    }
+  }, [onSetFileLoadingState]);
+
+  useEffect(() => {
+    if (loadingState === 'started') {
+      startProgressBarAnimation();
+    }
+  }, [loadingState, startProgressBarAnimation]);
 
   return (
     <div className="file-loading_input-wrapper">
       <img className="file-loading_jpg-icon" src="img/jpg-icon.png" alt="jpg-icon" />
       <div className="file-loading_info-wrapper">
-        <div className="file-loading_file-name">Photo 01-02-2021=20-33</div>
-        <progress className="file-loading_progress-bar" id="file-loading_progress-bar" value="60" max="100"></progress>
+        <div className={`file-loading_file-info${fileInfoClass}`}>{fileInfo}</div>
+        <progress className={`file-loading_progress-bar${progressBarClass}`} id="file-loading_progress-bar" value="0" max={MAX_PROGRESS}></progress>
       </div>
-      <img src="img/spiner-icon.png" alt="spiner" className="file-loading_spiner" hidden />
-      <div className="file-loading_pail"></div>
+      <img src="img/spiner-icon.png" alt="spiner" className={`file-loading_spiner${spinerVisibilityClass}`} />
+      <div className={`file-loading_pail${pailVisibilityClass}`} onClick={handlePailClick}></div>
     </div>
   );
 }
--- src/components/review-form/ReviewForm.jsx
+++ src/components/review-form/ReviewForm.jsx
@@ -4,27 +4,53 @@ import FileLoading from '../file-loading/FileLoading';
 
 const MIN_REVIEW_TEXT = 50;
 const MAX_REVIEW_TEXT = 200;
+const MAX_FILE_SIZE = 1000000;
 
 
 function ReviewForm(props) {
   const { onClose, onReturnSubmitResult } = props;
   const [userName, setUserName] = useState('');
+  const [files, setFiles] = useState([]);
   const [reviewText, setReviewText] = useState('');
   const [isFormValid, setIsFormValid] = useState(false);
+  const [fileLoadingState, setFileLoadingState] = useState('not started');
 
-  const handleInfoButtonClick = () => {
-    alert('Здесь будет ссылка на дополнительную информацию');
+  const cancelChoosingFile = () => {
+    setFileLoadingState('not started');
+    setFiles([]);
+    const inputFileElement = document.getElementById('choosing-file');
+    inputFileElement.value = '';
   };
 
-  const handleReviewTextChange = ({ target }) => {
-    setReviewText(target.value);
+  const validateFile = (files) => {
+    const fileSize = files[0].size;
+    if (fileSize > MAX_FILE_SIZE) {
+      setFileLoadingState('failed');
+    }
+  };
+
+  const handleChoosingFile = (evt) => {
+    try {
+      setFileLoadingState('started');
+      const data = evt.target.files;
+      if (data.length && data.length > 0) {
+        setFiles(data);
+        validateFile(data);
+      } else {
+        throw new Error('Choosing file failed');
+      }
+    } catch (e) {
+      cancelChoosingFile();
+    }
   };
 
   const handleFormSubmit = (evt) => {
     evt.preventDefault();
-    alert(`user name: ${userName}\nreview text: ${reviewText}\n`);
+    const fileName = files.length > 0 ? files[0].name : files;
+    alert(`user name: ${userName}\nfilename: ${fileName}\nreview text: ${reviewText}\n`);
     try {
       setUserName('');
+      setFiles([]);
       setReviewText('');
       if (isFormValid) {
         onReturnSubmitResult('success');
@@ -38,6 +64,10 @@ function ReviewForm(props) {
     }
   };
 
+  const handleInfoButtonClick = () => {
+    alert('Здесь будет ссылка на дополнительную информацию');
+  };
+
   const handleInputBlur = () => {
     const inputLabelElement = document.getElementById('choosing-file-label');
     inputLabelElement.classList.remove('focus');
@@ -46,6 +76,10 @@ function ReviewForm(props) {
   const handleInputFocus = () => {
     const inputLabelElement = document.getElementById('choosing-file-label');
     inputLabelElement.classList.add('focus');
+  };
+
+  const handleReviewTextChange = ({ target }) => {
+    setReviewText(target.value);
   };
 
   const handleUserNameChange = ({ target }) => {
@@ -73,13 +107,13 @@ function ReviewForm(props) {
       <label htmlFor="author-name" className="review-form_author-label">Как вас зовут?</label>
       <div className="review-form_authore-info">
         <input className="review-form_authore-input" type="text" name="author-name" id="author-name" placeholder="Имя Фамилия" title="Поле должно содержать имя и фамилию" required value={userName} onChange={handleUserNameChange} />
-        <input type="file" name="file" id="choosing-file" className="review-form_input-file-field" accept=".jpg, .jpeg" onFocus={handleInputFocus} onBlur={handleInputBlur} />
+        <input type="file" name="file" id="choosing-file" className="review-form_input-file-field" accept=".jpg, .jpeg" onFocus={handleInputFocus} onBlur={handleInputBlur} onChange={handleChoosingFile}  disabled={files.length > 0} />
         <label htmlFor="choosing-file" id="choosing-file-label" className="review-form_load-photo-button button">
           <img src="img/white-plus.png" alt="Плюс" />
           <span className="review-form_load-photo-button-label">Загрузить фото</span>
         </label>
       </div>
-      <FileLoading />
+      {fileLoadingState !== 'not started' && <FileLoading loadingState={fileLoadingState} files={files} onCancelChoosingFile={cancelChoosingFile} onSetFileLoadingState={setFileLoadingState}/>}
       <label htmlFor="author-text" className="review-form_text-label">Все ли вам понравилось?</label>
       <div className="review-form_review-text-wrapper">
         <textarea className="review-form_review-text" id="author-text" placeholder="Напишите пару слов о вашем опыте..." maxLength={MAX_REVIEW_TEXT} required value={reviewText} onChange={handleReviewTextChange}></textarea>
