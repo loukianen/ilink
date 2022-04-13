07f41192719f024b9ade03d6ce5fb859ed8d5f85
Konstantin Lukyanenok <loukianen@gmail.com>
2022-04-13 12:12:39 +0300

    build

--- src/components/app/App.test.js
+++
@@ -1,7 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders my name', () => {
-  render(<App />);
-  expect(screen.getByText(/Константин/i)).toBeInTheDocument();
-});
